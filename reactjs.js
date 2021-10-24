1ece8e97eb2b4962c93a7ba59ed266f9928994eb
Wallys L Souza <wallys31.souza@gmail.com>
2021-10-24 13:27:58 -0300

    Created first reactjs schedule

---
+++ reactjs.js
@@ -0,0 +1,25 @@
+const estudo = {
+  Semana: {
+    Segunda: {
+      Estudos: '06:30 - 08:00',
+    },
+    Terça: {
+      Estudos: '06:30 - 08:00', 
+    },
+    Quarta: {
+      Estudos: '06:30 - 08:00',
+    },
+    Quinta: {
+      Estudos: '06:30 - 08:00', 
+    },
+    Sexta: {
+      Estudos: '06:30 - 08:00',
+    },
+    Sabado: {
+      Estudos: '06:30 - 08:00',
+    },
+    Domingo: {
+      Estudos: '06:30 - 08:00',
+    },
+  }
+};
